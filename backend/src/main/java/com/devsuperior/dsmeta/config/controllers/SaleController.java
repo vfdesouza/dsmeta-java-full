033c6392a7b5eccc7511ec5c60423318fc2d8593
vfdesouza <vfdesouza@outlook.com>
2022-07-18 20:31:17 -0300

    Heroku

--- backend/src/main/java/com/devsuperior/dsmeta/config/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/config/controllers/SaleController.java
@@ -1,20 +1,20 @@
 package com.devsuperior.dsmeta.config.controllers;
 
+import com.devsuperior.dsmeta.config.services.SMSService;
 import com.devsuperior.dsmeta.config.services.SaleService;
 import com.devsuperior.dsmeta.entities.Sale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
     @Autowired
-    SaleService ss;
+    private SaleService ss;
+    @Autowired
+    private SMSService smsService;
 
     @GetMapping
     public Page<Sale> findSales(
@@ -23,4 +23,9 @@ public class SaleController {
             Pageable pageable) {
         return ss.findSales(minDate, maxDate, pageable);
     }
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
+    }
+
 }
---
+++ backend/src/main/java/com/devsuperior/dsmeta/config/services/SMSService.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dsmeta.config.services;
+
+import com.devsuperior.dsmeta.config.repositories.SaleRepository;
+import com.devsuperior.dsmeta.entities.Sale;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SMSService {
+
+    @Autowired
+    private SaleRepository sr;
+    @Value("${twilio.sid}")
+    private String twilioSid;
+
+    @Value("${twilio.key}")
+    private String twilioKey;
+
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
+
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+
+    public void sendSms(Long saleId) {
+
+        Sale sale = sr.findById(saleId).get();
+
+        String msg = "Vendedor " + sale.getSellerName();
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, msg).create();
+
+        System.out.println(message.getSid());
+    }
+}
